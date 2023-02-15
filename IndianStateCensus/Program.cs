2421c23f7bfcd1d4284fa0d3799647dc77e1eae1
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-15 17:58:42 +0530

    [Shruti] Perform Indian states census analyser program

---
+++ IndianStateCensus/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianStateCensus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian state census analyser problem");
+        }
+    }
+}
